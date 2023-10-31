9e93e5bddfc118371fe6203cb8e02b94d3ebe7af
Krishna Saxena <rio17105@gmail.com>
2023-10-31 17:40:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+function Billinput({ bill, onSetBill }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>How much was the bill? </p>
+      <input
+        type="number"
+        name="billinput"
+        id="billinput"
+        placeholder="Bill Amount"
+        value={bill}
+        onChange={(e) => onSetBill(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+function Serviceinp({ selfserv, onSetServ }) {
+  return (
+    <div>
+      <p>How did you like the service?</p>
+      <select
+        name="serviceinp"
+        id="servinp"
+        onChange={(e) => onSetServ(Number(e.target.value))}
+        value={selfserv}
+      >
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function FriendServiceinp({ fserv, onsetfServ }) {
+  return (
+    <div>
+      <p>How did your friend like the service?</p>
+      <select
+        name="Fserviceinp"
+        id="Fservinp"
+        onChange={(e) => onsetfServ(Number(e.target.value))}
+        value={fserv}
+      >
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
     </div>
   );
 }
 
-export default App;
+function FinalBill({ bill, total, tip }) {
+  return (
+    <div>
+      <h1>
+        You pay ${total} (${bill}+${tip} tip)
+      </h1>
+    </div>
+  );
+}
+function Reset({ onClick }) {
+  return (
+    <div>
+      <button className="reset" onClick={onClick}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default function App() {
+  const [bill, setBill] = useState("");
+  const [selfserv, setSelfServ] = useState(0);
+  const [fserv, setfServ] = useState(0);
+
+  const tipPercentageavg = (fserv + selfserv) / 2;
+  const tip = (bill / 100) * tipPercentageavg;
+  const total = tip + bill;
+
+  function resetbuttonhandler() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete the bill ? "
+    );
+
+    if (confirmed) {
+      setBill("");
+      setSelfServ(0);
+      setfServ(0);
+    }
+  }
+
+  return (
+    <div>
+      <Billinput bill={bill} onSetBill={setBill} />
+      <Serviceinp selfserv={selfserv} onSetServ={setSelfServ} />
+      <FriendServiceinp fserv={fserv} onsetfServ={setfServ} />
+      <FinalBill bill={bill} total={total} tip={tip} />
+      <Reset onClick={resetbuttonhandler} />
+    </div>
+  );
+}
